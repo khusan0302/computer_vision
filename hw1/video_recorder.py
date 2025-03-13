89c0f0705ad539c9bf3e4489ac57dc0087290723
Feruz Iskandarov <feruzbek.iskandarov@gmail.com>
2025-03-13 18:04:29 +0900

    made camera opener and save as mp4

--- hw1/video_recorder.py
+++ hw1/video_recorder.py
@@ -1,2 +1,12 @@
 import cv2 as cv
-print(cv.__version__)
+
+video = cv.VideoCapture(0)
+
+if not video.isOpened():
+    print("Cannot find the camera")
+    exit()
+frame_width = int(video.get(cv.CAP_PROP_FRAME_WIDTH))
+frame_height = int(video.get(cv.CAP_PROP_FRAME_HEIGHT))
+
+fourcc = cv.VideoWriter_fourcc(*'mp4v')
+out = cv.VideoWriter('video_output.mp4', fourcc, 20.0,(frame_width,frame_height))
